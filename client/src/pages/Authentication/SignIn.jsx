dfe7e34671ba3fb1bdfbcde6dc78b4d5eedcd8f5
PrasanthYT <jprasanth2006@gmall.com>
2024-08-23 14:02:10 +0530

    Completed All register pages frontend and solve the bugs.

--- client/src/pages/Authentication/SignIn.jsx
+++ client/src/pages/Authentication/SignIn.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LogoDark from "../../images/logo/logo-dark.svg";
-import Logo from "../../images/logo/logo.svg";
+import LogoDark from "../../images/logo/logoDark.png";
+import LogoLight from "../../images/logo/logoLight.png";
 import { useForm } from "react-hook-form";
 
 const SignIn = () => {
-  const { register, handleSubmit, formState: { errors }, watch } = useForm();
-
-  const password = watch('password');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -19,16 +21,16 @@ const SignIn = () => {
     <>
       <div className="rounded-sm border-stroke bg-white dark:border-strokedark dark:bg-boxdark">
         <div className="flex flex-wrap items-center">
-          <div className="hidden w-full xl:block xl:w-1/2">
+          <div className="hidden w-max xl:block xl:w-1/2">
             <div className="py-17.5 px-26 text-center">
               <Link className="mb-5.5 inline-block" to="/">
-                <img className="hidden dark:block" src={Logo} alt="Logo" />
-                <img className="dark:hidden" src={LogoDark} alt="Logo" />
+                <img className="hidden dark:block w-25" src={LogoLight} alt="Logo" />
+                <img className="dark:hidden w-25" src={LogoDark} alt="Logo" />
               </Link>
 
               <p className="2xl:px-20">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                suspendisse.
+                Unlock the Power of Hassle-Free GST Compliance with
+                Comprehensive Inventory and Invoice Tools.
               </p>
 
               <span className="mt-15 inline-block">
@@ -156,8 +158,8 @@ const SignIn = () => {
             </div>
           </div>
 
-          <div className="w-full xl:w-1/2">
-            <div className="py-17.5 px-10">
+          <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
+            <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
               <h4 className="mb-11 text-3xl font-bold text-black dark:text-black">
                 Welcome Back
               </h4>
@@ -173,17 +175,22 @@ const SignIn = () => {
                   <div className="relative">
                     <input
                       type="text"
-                      {...register('gstin', {
-                        required: 'GSTIN is required',
+                      {...register("gstin", {
+                        required: "GSTIN is required",
                         pattern: {
                           value: /^[a-zA-Z0-9]{15}$/,
-                          message: 'GSTIN must be exactly 15 alphanumeric characters',
+                          message:
+                            "GSTIN must be exactly 15 alphanumeric characters",
                         },
                       })}
                       placeholder="Enter GSTIN No"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                     />
-                    {errors.gstin && <p className="text-red-500">{errors.gstin.message}</p>}
+                    {errors.gstin && (
+                      <p className="text-red-500 text-sm mt-2">
+                        {errors.gstin.message}
+                      </p>
+                    )}
                     <span className="absolute right-4 top-4">
                       <svg
                         className="fill-current"
@@ -218,17 +225,17 @@ const SignIn = () => {
                   <div className="relative">
                     <input
                       type="password"
-                      {...register('password', {
-                        required: 'Password is required',
-                        pattern: {
-                          value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{10}$/,
-                          message: 'Password must be exactly 10 characters long, with at least one lowercase letter, one uppercase letter, and one numeric digit',
-                        },
+                      {...register("password", {
+                        required: "Password is required",
                       })}
                       placeholder="Enter Password"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                     />
-                    {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+                    {errors.password && (
+                      <p className="text-red-500 text-sm mt-2">
+                        {errors.password.message}
+                      </p>
+                    )}
                     <span className="absolute right-4 top-4">
                       <svg
                         className="fill-current"
@@ -238,7 +245,6 @@ const SignIn = () => {
                         fill="none"
                         xmlns="http://www.w3.org/2000/svg"
                       >
-                        
                         <g opacity="0.5">
                           <path
                             d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
@@ -252,15 +258,6 @@ const SignIn = () => {
                       </svg>
                     </span>
                   </div>
-                </div>
-
-                <div className="mb-6 flex items-center justify-between">
-                  <Link
-                    to="/"
-                    className="text-base font-medium text-primary hover:underline dark:text-black"
-                  >
-                    Forgot Password?
-                  </Link>
                 </div>
 
                 <button
