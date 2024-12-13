39094f812e56ee6975cbaddcbd280ded826ceeba
PrasanthYT <jprasanth2006@gmail.com>
2024-12-14 04:04:23 +0530

    Updated Localhost to Render Link

--- client/src/pages/Authentication/Signup.jsx
+++ client/src/pages/Authentication/Signup.jsx
@@ -24,7 +24,7 @@ const SignUp = () => {
     try {
       // Make sure to use the correct signup API endpoint
       const response = await axios.post(
-        "http://localhost:8000/auth/signup", // Update this if your endpoint is different
+        "http://gstsync.onrender.com/auth/signup", // Update this if your endpoint is different
         data,
         {
           headers: {
